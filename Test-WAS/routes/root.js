fdc2a99e1f29bfe31c7f39b2b2cf85b2addc72e4
Sondoobo <style056811@gmail.com>
2023-01-21 23:00:19 +0900

    modified root js file

--- Test-WAS/routes/root.js
+++ Test-WAS/routes/root.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: "오늘 하루는 너무 심심해" }
+    return { root: "오늘 너무 심심해 ㅠㅠ" }
   })
 }
